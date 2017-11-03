ae5feaff0cdef5fcffc96644cc77b201a9ecdb40
Pablo <paul.ladiv123@gmail.com>
2017-11-03 13:53:20 +0000

    small change

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+app.use(passport.initialize());
+app.use(passport.session());
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render("login");
